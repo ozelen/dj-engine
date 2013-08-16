ded4f99814fd1f88ad4118655a11cd6ee4d574b4
Oleksiy Zelenyuk <oleksadesign@gmail.com>
2013-08-16 09:07:15 +0400

    connect a photo to a gallery

--- app/assets/javascripts/photos.js.coffee
+++ app/assets/javascripts/photos.js.coffee
@@ -1,8 +1,11 @@
 
 jQuery ->
+  data = $('#new_photo').serializeArray()
+  console.log data
   $('#new_photo').fileupload
       dataType: "script"
       paramName: 'photo[image]'
+      formData: data
       progressall: (e, data) ->
         progress = parseInt(data.loaded / data.total * 100, 10)
         $('#progress .bar').css(
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,4 +1,6 @@
 class Gallery < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
-  attr_accessible :cover_photo_id, :description, :imageable_id, :imageable_type, :name
+  has_many :photos
+  accepts_nested_attributes_for :photos
+  attr_accessible :cover_photo_id, :description, :imageable_id, :imageable_type, :name, :photos_attributes
 end
