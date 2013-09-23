58129b6a607fc103d069cacd1fec0807674fd51b
Oleksiy Zelenyuk <oleksadesign@gmail.com>
2013-09-24 00:15:33 +0300

    hack for heroku pg

--- db/migrate/20130901045551_change_field_id_type.rb
+++ db/migrate/20130901045551_change_field_id_type.rb
@@ -1,11 +1,15 @@
 class ChangeFieldIdType < ActiveRecord::Migration
   def up
-    change_column :values, :field_id, :integer
-    change_column :values, :measure_id, :integer
+    #change_column :values, :field_id, :integer # doesn't work on heroku
+    #change_column :values, :measure_id, :integer
+    remove_column :values, :field_id
+    remove_column :values, :measure_id
+    add_column :values, :field_id, :integer
+    add_column :values, :measure_id, :integer
   end
 
   def down
-    change_column :values, :field_id, :string
-    change_column :values, :measure_id, :string
+    #change_column :values, :field_id, :string
+    #change_column :values, :measure_id, :string
   end
 end
