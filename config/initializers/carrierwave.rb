25b0825ce600bee7efa9ee67b770ee31be19ba93
Oleksiy Zelenyuk <oleksadesign@gmail.com>
2013-10-21 16:39:00 +0300

    change bucket back

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -30,11 +30,11 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
       persistent: false,
       provider: "AWS",
-      region: 'eu-west-1',
+      #region: 'eu-west-1',
       aws_access_key_id: 'AKIAJD553Y7CKLHWQTFA', # S3_CONFIG['key'],
       aws_secret_access_key: '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' # S3_CONFIG['secret'],
   }
-  config.fog_directory = 'djerelo-storage' #S3_CONFIG['bucket']
+  config.fog_directory = 'djerelo' #S3_CONFIG['bucket']
   #config.fog_public     = true
   #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
