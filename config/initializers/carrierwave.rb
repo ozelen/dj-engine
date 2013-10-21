432fb42f3ed896800d639ab74683c624f8f870a0
Oleksiy Zelenyuk <oleksadesign@gmail.com>
2013-10-21 14:42:14 +0400

    change bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,5 +5,5 @@ CarrierWave.configure do |config|
       aws_access_key_id: 'AKIAJD553Y7CKLHWQTFA', # S3_CONFIG['key'],
       aws_secret_access_key: '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' # S3_CONFIG['secret']
   }
-  config.fog_directory = 'djerelo' #S3_CONFIG['bucket']
+  config.fog_directory = 'djerelo-storage' #S3_CONFIG['bucket']
 end
