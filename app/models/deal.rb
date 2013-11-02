69c21d6e503fcd7ffb73b8296bb2722dfa73bf8e
Oleksiy Zelenyuk <oleksadesign@gmail.com>
2013-11-02 22:49:49 +0200

    update hotel ordering fields on deals update

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,4 +1,13 @@
 class Deal < ActiveRecord::Base
   belongs_to :dealable, polymorphic: true
   attr_accessible :begin_date, :contacts, :dealable_id, :dealable_type, :expire_date, :login, :password, :person_name, :price, :status
+
+  after_update :update_rating
+
+  def update_rating
+    dealable.donated      = dealable.deals.sum(:price)
+    dealable.deal_expire  = dealable.deals.maximum(:expire_date) || '2006-04-20'.to_date
+    dealable.save!
+  end
+
 end
