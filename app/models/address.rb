af4be85ecddda1e57d96db32b8a3f95bfc14e107
Oleksiy Zelenyuk <oleksadesign@gmail.com>
2013-11-13 21:42:28 +0200

    translate addresses model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
-  attr_accessible :address, :addressable_id, :addressable_type, :email, :fax, :phone1, :phone2, :phone3, :skype, :website
+  attr_accessible :addr, :addressable_id, :addressable_type, :email, :fax, :phone1, :phone2, :phone3, :skype, :website
+  translates :addr
 end
