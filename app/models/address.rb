63a3bc8924c4b6c0a2fb828f311bfae8a00d4485
Oleksiy Zelenyuk <oleksadesign@gmail.com>
2013-11-16 18:41:09 +0200

    fixes

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,8 +3,6 @@ class Address < ActiveRecord::Base
   attr_accessible :addr, :addressable_id, :addressable_type, :email, :fax, :phone1, :phone2, :phone3, :skype, :website
   translates :addr
 
-  before_save :to_markdown
-
   def to_markdown!
     self.addr = ReverseMarkdown.parse_string addr
   end
