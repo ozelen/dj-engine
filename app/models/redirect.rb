0f0010f07a1acd46964653b0d5d22030a6623f75
Oleksiy Zelenyuk <oleksiy.zelenyuk@symphony-solutions.eu>
2016-07-25 14:25:55 +0200

    Get SkiWorld back

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -8,7 +8,7 @@ class Redirect < ActiveRecord::Base
     redirect = Redirect.where(old_path: old_path, old_domain: request.host)[0] # looking for existing redirect on the path in DB (locale should be undefined!)
     locale = locale_reg.match(request.fullpath).to_s.gsub('ua', 'uk') # find out the locale and substitute ua with uk identifier
     locale = '' if locale == '/ru' # no specified locale if default
-    "http://besthotels.in.ua#{locale}#{redirect.new_path}" if redirect
+    "http://skiworld.org.ua#{locale}#{redirect.new_path}" if redirect
 
   end
 
